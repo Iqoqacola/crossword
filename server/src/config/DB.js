b8a254b1ccb24a7a219b40068a585d6ee34d374c
Iqoqacola <iqoqacola@gmail.com>
2026-03-04 18:07:21 +0800

    fix: resolve database connection timeout and idle crash on railway

--- server/src/config/DB.js
+++ server/src/config/DB.js
@@ -11,12 +11,23 @@ const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
+    port: DB_PORT || 3306,
     dialect: 'mysql',
-    port: process.env.DB_PORT || 3306,
     logging: false,
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+    dialectOptions: {
+        connectTimeout: 60000
+    }
 });
 
 module.exports = sequelize;
